15790b4cd443dbc3bcb0580665f91fff4344bb03
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-05-23 14:16:24 +0300

    pluse cheak

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+// jsx
+const App = () => {
+  const todo = [{todo: "wake up", id: 1}, {todo: "breakfast", id: 2}, {todo: "study", id: 3}];
+  return <h1>hello world</h1>;
+};
+
+export default App
